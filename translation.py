433f6fba18e354b2777e78969250e0adb44d2fbe
Ytstudio <91581305+ytcreatorstudio2001@users.noreply.github.com>
2022-02-05 11:20:31 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -11,8 +11,8 @@ URL | filename | username | password"""
     DOWNLOAD_START = "📥Downloading..."
     UPLOAD_START = "📤Uploading..."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using me \n\n<b>Join @TMWAD For More UsefUl Bots Like Me </b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@TMWAD"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using me \n\n<b>Join @MovieHub_2k22 For More UsefUl Bots Like Me </b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@MovieHub_2k22"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     CUSTOM_CAPTION_UL_FILE = "{}"
@@ -21,7 +21,7 @@ URL | filename | username | password"""
     
    ☞My Name  : All Url Uploader Bot
 
-   ☞Updates  : @TMWAD    
+   ☞Updates  : @MovieHub_2k22    
 
    ☞Language : Python3
 
@@ -36,7 +36,7 @@ URL | filename | username | password"""
    Video  - Give File as video without Screenshots
    File   - Give File without Screenshots
 
-If bot didn't respond, Ask Here @TMWAD"""
+If bot didn't respond, Ask Here @akhil_tg"""
     REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
 You can use /rename command after receiving file to rename it with custom thumbnail support.
